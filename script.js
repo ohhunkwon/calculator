61dd97ecbf6a9e61c43b73511ceeb1aa8f0a6fd4
Ohhun Kwon <ohhunkwon@hotmail.com>
2021-07-08 09:04:24 +0100

    AC button functionality and zero division error fix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const numbersBtn = document.querySelectorAll(".number")
 const operatorsBtn = document.querySelectorAll(".operator")
 const equalsBtn = document.querySelector(".equals")
+const clearBtn = document.querySelector(".ac")
 const screen = document.querySelector(".screen")
 
 let firstNum
@@ -11,7 +12,6 @@ let result
 numbersBtn.forEach(numberBtn => {
     numberBtn.addEventListener('click', (e) => {
         if (screen.textContent.length > 11 && operators.length === 0) {
-            console.log("we've returned")
             return
         }
         if (operators.length > 0 && !secondNum) {
@@ -35,6 +35,12 @@ operatorsBtn.forEach(operatorBtn => {
         if (operators.length > 0) {
             operator = operators.pop()
             result = operate(operator, firstNum, secondNum)
+            if (result === "divide by zero error") {
+                screen.textContent = "Error"
+                firstNum = null
+                secondNum = null
+                return
+            }
             screen.textContent = result
             firstNum = result
             secondNum = null
@@ -42,19 +48,34 @@ operatorsBtn.forEach(operatorBtn => {
         } else if (operators.length === 0) {
             operators.push(e.target.dataset.operator)
         }
-        console.log(operators)
     })
 })
 
 equalsBtn.addEventListener('click', (e) => {
-    if (!secondNum) return
+    if (secondNum === null) {
+        return
+    }
     if (operators.length > 0) {
         operator = operators.pop()
         result = operate(operator, firstNum, secondNum)
+        if (result === "divide by zero error") {
+            screen.textContent = "Error"
+            firstNum = null
+            secondNum = null
+            return
+        }
         screen.textContent = result
         firstNum = result
         secondNum = null
     }
+})
+
+clearBtn.addEventListener('click', () => {
+    firstNum = null
+    secondNum = null
+    operators = []
+    result = null
+    screen.textContent = ""
 })
 
 function add(a, b) {
@@ -82,7 +103,7 @@ function operate(operator, a, b) {
         case "×":
             return multiply(a, b)
         case "÷":
-            if (b === 0) return null
+            if (b === 0) return 'divide by zero error'
             return divide(a, b)
     }
 }
